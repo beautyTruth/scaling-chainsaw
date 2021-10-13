0cb7af2e2924f9a4034d02d7b13f77b47dab5073
Phil <happy.melodist@gmail.com>
2021-10-13 01:30:49 -0500

    correct bug in JavaScript and complete .change class styling

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 Array.from(document.querySelectorAll(".navigation-button")).forEach(
   (boobie) => {
     boobie.onclick = () => {
-      boob.parentElement.parentElement.classList.toggle("change");
+      boobie.parentElement.parentElement.classList.toggle("change");
     };
   }
 );
